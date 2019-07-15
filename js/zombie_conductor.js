0ab1ff8d68e22002511918672c9e1bcbe0b02b63
eduardomgonzalez <44064190+eduardomgonzalez@users.noreply.github.com>
2019-07-15 18:48:33 -0300

    Primera carga

---
+++ js/zombie_conductor.js
@@ -0,0 +1,40 @@
+/* Para insipirarte para la implementacion del ZombieConductor podes usar
+al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
+Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
+no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
+del ZombieCaminante que eran los mismos. */
+
+var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov, direccion) {
+  /* Completar constructor a partir de Enemigo */
+  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);
+  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+  this.direccion = direccion;
+}
+/* Completar creacion del ZombieConductor */
+ZombieConductor.prototype = Object.create(Enemigo.prototype);
+ZombieConductor.prototype.constructor = ZombieConductor;
+
+/* Completar metodos para el movimiento y el ataque */
+ZombieConductor.prototype.mover = function () {
+
+  if (this.direccion === 'h') {
+    this.x -= this.velocidad;
+  }else {
+    this.y -= this.velocidad;
+  }
+
+  /* En esta parte lo que hacemos es invertir la direccion horizontal si
+  toca uno de sus limites, modificando su velocidad. Si multiplicamos por -1 la
+  velocidad lo que estamos haciendo es invertir su direccion.*/
+  if ((this.x < this.rangoMov.desdeX) || (this.x > this.rangoMov.hastaX)){
+    this.velocidad *= -1;
+  }
+  // Aca hago lo mismo que en la direccion horizontal (invierte la direccion)
+  if ((this.y < this.rangoMov.desdeY) || (this.y > this.rangoMov.hastaY)) {
+    this.velocidad *= -1;
+  }
+}
+
+ZombieConductor.prototype.atacar = function(jugador) {
+  jugador.perderVidas(5);
+}
